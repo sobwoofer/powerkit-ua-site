1513065bc3412d41eb1422c7778c8e05ca5fc8ab
lex <savchenko.lex@gmail.com>
2023-06-08 02:23:59 +0300

    add swiper lib for slider

---
+++ js/swiper.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+  spaceBetween: 100,
+  allowSlideNext: true,
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+  // Now you can use all slider methods like
+// swiper.slideNext();
--- partials/head.php
+++ partials/head.php
@@ -7,6 +7,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" type="image/x-icon" href="../favicon-32x32.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"
+/>
+
 <?php 
 function addCss(string $css_file_name = 'styles.css'){
     $file_with_path = $_SERVER['DOCUMENT_ROOT'] . "/css/" . $css_file_name;
@@ -30,10 +40,5 @@ addCss('footer.css');
 ?>
     
     <title>Powerkit</title>
-    <link rel="icon" type="image/x-icon" href="../favicon-32x32.png">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <script src="../js/burger-menu.js" type="module"></script>
-    <script src="../js/counter.js" type="module"></script>
+   
 </head>
