74c346553091da79d91017d843cd6c63cd925b76
lex <savchenko.lex@gmail.com>
2023-06-08 06:22:40 +0300

    customize swiper, add css style

--- js/swiper.js
+++ js/swiper.js
@@ -1,26 +1,48 @@
 const swiper = new Swiper('.swiper', {
     speed: 400,
-  spaceBetween: 100,
   allowSlideNext: true,
-    // Optional parameters
-    // direction: 'vertical',
+  allowSlidePrev: true,
     loop: true,
   
-    // If we need pagination
-    // pagination: {
-    //   el: '.swiper-pagination',
-    // },
-  
-    // Navigation arrows
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
-  
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
+    breakpoints: {
+        // when window width is >= 320px
+        375: {
+          slidesPerView: 2.5,
+          spaceBetween: 8
+        },
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 1.25,
+          spaceBetween: 12
+        },
+        // when window width is >= 640px
+        1440: {
+          slidesPerView: 1.5,
+          spaceBetween: 24
+        }
+    },
   });
-  // Now you can use all slider methods like
-// swiper.slideNext();
+
+  const swiperReview = new Swiper('.swiper-review',{
+    speed: 400,
+  allowSlideNext: true,
+  allowSlidePrev: true,
+    loop: true,
+    breakpoints: {
+        // when window width is >= 320px
+        375: {
+          slidesPerView: 2.25,
+          spaceBetween: 16
+        },
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 2.25,
+          spaceBetween: 12
+        },
+        // when window width is >= 640px
+        1440: {
+          slidesPerView: 4,
+          spaceBetween: 24
+        }
+    },
+  })
