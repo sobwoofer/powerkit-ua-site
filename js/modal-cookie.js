5fb23c8f58348d1b81a49f474b5561f70695a7b6
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-06-09 13:43:38 +0300

    Add agree button

--- js/modal-cookie.js
+++ js/modal-cookie.js
@@ -1,13 +1,20 @@
 const cookieMenu = document.querySelector(".modal-cookie");
 const closeMenuBtn = document.querySelector(".modal-button-close");
+const agreeBtn = document.querySelector(".modal-content-btn");
 
+agreeBtn.addEventListener("click", setCookie);
+closeMenuBtn.addEventListener("click", closeCookieMenu);
 
 setTimeout(function openCookieMenu() {
   cookieMenu.classList.remove("visually-hidden");
 }, 5000);
-document.cookie = `PKCoockiesAgree=${Date.now()}; expires=Thu, 18 Dec 2024 12:00:00 UTC;`;
 
-closeMenuBtn.addEventListener("click", closeCookieMenu);
+function setCookie(event) {
+  event.preventDefault();
+  cookieMenu.classList.add("visually-hidden");
+  document.cookie = `PKCoockiesAgree=${Date.now()}; expires=Thu, 18 Dec 2024 12:00:00 UTC;`;
+}
+
 function closeCookieMenu(event) {
   event.preventDefault();
   cookieMenu.classList.add("visually-hidden");
