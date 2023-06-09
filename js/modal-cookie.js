9d036f2df9c325f895cdd6a5be0d138f24228926
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-06-09 13:47:01 +0300

    added info about g.analytics

--- js/modal-cookie.js
+++ js/modal-cookie.js
@@ -5,14 +5,28 @@ const agreeBtn = document.querySelector(".modal-content-btn");
 agreeBtn.addEventListener("click", setCookie);
 closeMenuBtn.addEventListener("click", closeCookieMenu);
 
-setTimeout(function openCookieMenu() {
-  cookieMenu.classList.remove("visually-hidden");
-}, 5000);
+if (document.cookie.PKCoockiesAgree) {
+  setTimeout(function openCookieMenu() {
+    cookieMenu.classList.remove("visually-hidden");
+  }, 5000);
+}
 
 function setCookie(event) {
   event.preventDefault();
   cookieMenu.classList.add("visually-hidden");
   document.cookie = `PKCoockiesAgree=${Date.now()}; expires=Thu, 18 Dec 2024 12:00:00 UTC;`;
+  // i додати скрипт у тіло сторінки
+  //   Тег для Google Analytics
+
+  // <!-- Google tag (gtag.js) -->
+  // <script async src="https://www.googletagmanager.com/gtag/js?id=G-VK2FJHKC8D"></script>
+  // <script>
+  //   window.dataLayer = window.dataLayer || [];
+  //   function gtag(){dataLayer.push(arguments);}
+  //   gtag('js', new Date());
+
+  //   gtag('config', 'G-VK2FJHKC8D');
+  // </script>
 }
 
 function closeCookieMenu(event) {
