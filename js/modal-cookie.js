2ce85eec0f15b0e7a07069d1d5805ce8ff1cc4ab
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-06-09 15:43:31 +0300

    Added cookie function

--- js/modal-cookie.js
+++ js/modal-cookie.js
@@ -5,28 +5,37 @@ const agreeBtn = document.querySelector(".modal-content-btn");
 agreeBtn.addEventListener("click", setCookie);
 closeMenuBtn.addEventListener("click", closeCookieMenu);
 
-if (document.cookie.PKCoockiesAgree) {
+if (!document.cookie) {
   setTimeout(function openCookieMenu() {
     cookieMenu.classList.remove("visually-hidden");
   }, 5000);
 }
 
-function setCookie(event) {
-  event.preventDefault();
+function setCookie() {
+  // закрити модальне вікно з кукі
   cookieMenu.classList.add("visually-hidden");
-  document.cookie = `PKCoockiesAgree=${Date.now()}; expires=Thu, 18 Dec 2024 12:00:00 UTC;`;
-  // i додати скрипт у тіло сторінки
+
   //   Тег для Google Analytics
-
   // <!-- Google tag (gtag.js) -->
-  // <script async src="https://www.googletagmanager.com/gtag/js?id=G-VK2FJHKC8D"></script>
-  // <script>
-  //   window.dataLayer = window.dataLayer || [];
-  //   function gtag(){dataLayer.push(arguments);}
-  //   gtag('js', new Date());
 
-  //   gtag('config', 'G-VK2FJHKC8D');
-  // </script>
+  // i додати скрипт у тіло сторінки:
+  const string = `<script async src="https://www.googletagmanager.com/gtag/js?id=G-VK2FJHKC8D"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-VK2FJHKC8D');
+  </script>`;
+  document.body.insertAdjacentHTML("beforeend", string);
+
+  // встановити кукі
+  const d = new Date();
+  let cvalue = d.toUTCString();
+  d.setTime(d.getTime() + 365 * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
+  let cname = "PKCoockiesAgree";
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function closeCookieMenu(event) {
