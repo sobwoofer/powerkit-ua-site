f1583386a4ded2f3ed593d99641cbab9f61409c9
lex <savchenko.lex@gmail.com>
2023-06-12 15:50:00 +0300

    create pages for privacy & offer

---
+++ public-offer.php
@@ -0,0 +1,5 @@
+<?php include "./partials/head.php"; ?>
+<?php include "./partials/header.html" ;?>
+<?php include "./partials/text-offer.html";?>
+<?php include "./partials/section-contacts.html" ;?>
+<?php include "./partials/footer.html" ;?>
