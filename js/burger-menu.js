b6e6771e1bb2e74f8211046e1b4d14117b331962
lex <savchenko.lex@gmail.com>
2023-07-18 02:26:09 +0300

    from burger to cross btn on css

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,8 +1,10 @@
 const mobileMenu = document.querySelector(".js-menu-container");
 const openMenuBtn = document.querySelector(".js-open-menu");
+const burger = document.querySelector('.burger');
 
 openMenuBtn.addEventListener("click", toggleMenu);
 function toggleMenu(event) {
   event.preventDefault();
+  burger.classList.toggle('active');
   mobileMenu.classList.toggle("visually-hidden");
 }
