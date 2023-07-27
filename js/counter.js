f98241239f31543cdfdb0655182c675de024f7e9
lex <savchenko.lex@gmail.com>
2023-07-27 13:17:03 +0300

    create connect to google sheets

--- js/counter.js
+++ js/counter.js
@@ -57,3 +57,30 @@ function windowLoad() {
     });
   }
 }
+
+
+//https://docs.google.com/spreadsheets/d/1g7L0wGZOsrsTBnFKl4MQsw9O9fVYJ2XxAy0K9gFGSoU/edit?usp=sharing
+
+const gDocBaseURL = 'https://docs.google.com/spreadsheets/d/';
+const gDID = '1g7L0wGZOsrsTBnFKl4MQsw9O9fVYJ2XxAy0K9gFGSoU';
+const sheetName = 'Sheet1';
+const range = 'B5:C5';
+
+const accessLink = gDocBaseURL + gDID + '/gviz/tq?sheet=' + sheetName + '&range=' + range;
+
+console.log(accessLink);
+
+fetch(accessLink).then(response => 
+  response.text()).then( (data) => {
+  // console.dir(data);
+    const converter = JSON.parse(data.substring(47).slice(0,-2));
+  console.log(converter);
+  console.log(converter.table.rows[0].c[1].v);
+  const filds = document.querySelectorAll('[data-digits-couter]');
+  console.log(filds);
+  for(let i = 0; i < filds.length; i++){
+    console.log(filds[i]);
+    filds[i].dataset.max = converter.table.rows[0].c[i].v;
+  }
+  
+})
