686065c003767c46341d1b1346837365d78e8374
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-08-03 19:55:29 +0300

    Updated counter

--- js/counter.js
+++ js/counter.js
@@ -12,9 +12,8 @@ function windowLoad() {
     }
   }
 
- async function digitsCountersAnimate(digitsCounter) {
+  function digitsCountersAnimate(digitsCounter) {
     let startTimestamp = null;
-    await refreshDataCounters();
     const duration = parseInt(digitsCounter.dataset.digitsCounter)
       ? parseInt(digitsCounter.dataset.digitsCounter)
       : 1000;
@@ -48,7 +47,6 @@ function windowLoad() {
         }
       }
     });
-
   }, options);
 
   let sections = document.querySelectorAll(".amount-item-numbers");
@@ -56,38 +54,5 @@ function windowLoad() {
     sections.forEach((section) => {
       observer.observe(section);
     });
-  }
-}
-
-//https://docs.google.com/spreadsheets/d/1g7L0wGZOsrsTBnFKl4MQsw9O9fVYJ2XxAy0K9gFGSoU/edit?usp=sharing
-//https://docs.google.com/spreadsheets/d/1CEQsUVfJFMx7zCAHLm2KPGLSjaNSNxfMk7107ZH2ae8/edit#gid=0
-
-async function getNubers(){
-  const gDocBaseURL = 'https://docs.google.com/spreadsheets/d/';
-  const gDID = '1CEQsUVfJFMx7zCAHLm2KPGLSjaNSNxfMk7107ZH2ae8';
-  const sheetName = 'Sheet1';
-  const range = 'B5:C5';
-
-  const accessLink = gDocBaseURL + gDID + '/gviz/tq?sheet=' + sheetName + '&range=' + range;
-
-  try {
-    const response = await fetch(accessLink);
-    const res2text = await response.text();
-    return JSON.parse(res2text.substring(47).slice(0,-2));
-    
-  } catch (error) {
-    console.error(error);
-  }
-
-}
-
-async function refreshDataCounters(){
-
-  const data = await getNubers();
-
-  const filds = document.querySelectorAll('[data-digits-couter]');
-
-  for(let i = 0; i < filds.length; i++){
-    filds[i].dataset.max = data.table.rows[0].c[i].v;
   }
 }
