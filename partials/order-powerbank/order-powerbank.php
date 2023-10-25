7fb4e5bdb2a3f8b88ea5037e11b63905b7370c31
ivan <sobwoofer8@gmail.com>
2023-10-25 22:22:17 +0300

    fixed index-cb5ff997.js path

--- partials/order-powerbank/order-powerbank.php
+++ partials/order-powerbank/order-powerbank.php
@@ -1,3 +1,3 @@
 <div id="root"></div>
 
-<script type="module" crossorigin src="assets/index-cb5ff997.js"></script>
+<script type="module" crossorigin src="partials/order-powerbank/assets/index-cb5ff997.js"></script>
