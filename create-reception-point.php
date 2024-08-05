12aa17de918fc66768d354e386b3f1e40770a278
vladmertvychenko <vladyslav.mertvychenko@y-tree.com>
2024-08-05 17:39:21 +0300

    Added reception point page

---
+++ create-reception-point.php
@@ -0,0 +1,5 @@
+<?php include "./partials/head.php"; ?>
+<?php include "./partials/header.html" ;?>
+<?php include "./partials/create-reception-point/create-reception-point.php" ;?>
+<?php include "./partials/section-contacts.html" ;?>
+<?php include "./partials/footer.php" ;?>
---
+++ partials/create-reception-point/create-reception-point.php
@@ -0,0 +1,3 @@
+<div id="create-reception-point-form"></div>
+
+<script type="module" crossorigin src="https://this4you.github.io/powerkit-forms/powerkitForms.js"></script>
